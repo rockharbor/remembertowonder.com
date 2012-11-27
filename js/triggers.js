a4798bca26872458ce4096cec6a639e3bf2b975e
Jeremy Harris <jharris@rockharbor.org>
2012-11-26 16:11:17 -0800

    Added swipe(In|Out) transitions

--- js/triggers.js
+++ js/triggers.js
@@ -2,6 +2,96 @@
  * This file holds all the timeline trigger functions for the slides
  */
 
+/*= Transitions
+********************************************************/
+/**
+ * Clean LTR swipe that reveals the contents of `element`
+ * 
+ * @param HTMLElement element Element to reveal
+ */
+function swipeIn(element) {
+	element.show();
+	
+	var viewport;
+	if (element.parent('.viewport-contents').length === 0) {
+		viewport = _swipeSetupElement(element);
+	} else {
+		viewport = element.closest('.viewport');
+	}
+	
+	viewport.animate({
+		left: element.data('originalPos').left
+	}, 2000);
+	viewport.children('.viewport-contents').animate({
+		left: 0
+	}, 2000);
+}
+
+/**
+ * Clean RTL swipe that hides the contents of `element`
+ * 
+ * @param HTMLElement element Element to hide
+ */
+function swipeOut(element) {
+	element.show();
+	
+	var viewport;
+	if (element.parent('.viewport-contents').length === 0) {
+		viewport = _swipeSetupElement(element);
+		viewport.css({
+			left: element.data('originalPos').left
+		});
+		viewport.children('.viewport-contents').css({
+			left: 0
+		});
+	} else {
+		viewport = element.closest('.viewport');
+	}
+	
+	viewport.animate({
+		left: element.data('originalPos').left - element.width()
+	}, 2000);
+	viewport.children('.viewport-contents').animate({
+		left: element.width()
+	}, 2000);
+}
+
+/**
+ * Helper function for wrapping elements that use the swiping animation Defaults
+ * to positioning needed to swipeIn
+ *
+ * @param HTMLElement element Element that will be swiped
+ */
+function _swipeSetupElement(element) {
+	var pos = element.position();
+	element.data('originalPos', pos);
+	element.wrap('<div class="viewport-contents"></div>');
+	var swipe = $(element).parent('.viewport-contents');
+	swipe.wrap('<div class="viewport"></div>');
+	var viewport = swipe.parent('.viewport');
+	viewport.css({
+		position: 'absolute',
+		left: pos.left - element.width(),
+		top: pos.top,
+		overflow: 'hidden',
+		height: element.height(),
+		width: element.width()
+	});
+	swipe.css({
+		position: 'absolute',
+		left: element.width(),
+		top: 0,
+		height: element.height(),
+		width: element.width()
+	});
+	element.css({
+		position: 'absolute',
+		left: 0,
+		top: 0
+	});
+	return viewport;
+}
+
 /**
  * Slide 1 trigger functions
  */

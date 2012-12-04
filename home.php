b1d4bcd7f24f360655ab583ec266c66df2383923
Jeremy Harris <jharris@rockharbor.org>
2012-12-04 10:04:21 -0800

    Added ocean section and preliminary layout

--- home.php
+++ home.php
@@ -57,6 +57,21 @@
 					}
 				}
 				
+				Ocean = {
+					showSalt: function() {
+						$('.section-ocean .salt').animate({opacity: 1});
+					},
+					hideSalt: function() {
+						$('.section-ocean .salt').animate({opacity: 0});
+					},
+					showBeaker: function() {
+						$('.section-ocean .beaker').animate({opacity: 1});
+					},
+					hideBeaker: function() {
+						$('.section-ocean .beaker').animate({opacity: 0});
+					}
+				}
+				
 				$('[data-fade]').each(function() {
 					var el = this;
 					var pos = $(el).offset();
@@ -123,6 +138,23 @@
 					<p style="top: 200px;" class="stickright" data-fade='{"enter": "Earth.showFrozen"}'>A minor decrease in that distance and the effect is catastrophic. Glaciers would melt. Most of our cities would flood. Ocean area would increase before potentially boiling and evaporating entirely.</p>
 					<p style="top: 400px;" class="stickleft" data-fade='{"enter": "Earth.showCold"}'>A minor increase in that distance and the effect is also catastrophic. More glaciers means more reflection of the sun&apos;s heat. Colder ocean temperatures would trap and dissolve much of the atmosphere&apos;s carbon dioxide. A decrease in the presence of CO<sub>2</sub> could drop temperatures devastatingly low.</p>
 					<p style="top: 400px;" class="stickright" data-fade="true">Even the slightest movement of Earth, either toward or away from the sun, and life might not exist as we know it.</p>
+				</section>
+				<section class="section-ocean" style="height: 4000px">
+					<h1 data-fade="true">Have you ever thought about the ocean?</h1>
+					<div class="water"></div>
+					<img data-z="-500" class="land" src="<?php echo img('terrain-right.png'); ?>" />
+					<img data-z="-400" class="salt" src="<?php echo img('salt.png'); ?>" />
+					<p style="top: 200px">At 140,000,000 square miles, it occupies more than 70% of the Earth&apos;s surface.</p>
+					<p style="left: 150px; top: 400px;" data-fade="true">If you evaporated all its water and spread the resulting salt equally over the earth&apos;s land area, there would be a 500-foot layer that covered our continents.</p>
+					<p style="top: 600px; left: -300px; width: 600px;" data-fade="true">A single milliliter of ocean water may contain tens of thousands of zooplankton, hundreds of thousands of phytoplankton, millions of bacterial cells, and more than ten million viruses.</p>
+					<img data-z="-200" class="beaker" src="<?php echo img('beaker.png'); ?>" data-fade="true" />
+					<p style="top: 1100px; left: -150px; width: 300px;" data-fade="true">It provides 99% of the Earth&apos;s living space. More than 250,000 known species inhabit it.</p>
+					<img data-z="-100" style="left:0; top: 700px" src="<?php echo img('whale.png'); ?>" />
+					<img data-z="-5000" style="left:-3000px; top: 1000px" src="<?php echo img('whale.png'); ?>" />
+					<img data-z="-7000" style="left:4000px; top: 4000px" src="<?php echo img('whale.png'); ?>" />
+					<img data-z="-6500" style="left:-2000px; top: 6500px" src="<?php echo img('whale.png'); ?>" />
+					<p style="top: 1600px; left: -150px; width: 300px;" data-fade="true">And less than 10% has been explored by humans.</p>
+					<img data-z="-500" class="floor" src="<?php echo img('ocean-floor.png'); ?>" />
 				</section>
 			</div>
 		</div>
